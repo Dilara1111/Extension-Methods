5b8f712d2fcc0d2bc36cbd307a2fda4012cdfca6
HuseynovaDilara <huseynova.8885@gmail.com>
2023-11-14 16:28:07 +0400

    Task added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_14._11._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = new int[] {1,2,3,4 };
+            //arr = arr.Add(1);
+            //arr = arr.Add(2);
+            //arr = arr.Add(3);
+            //arr = arr.Add(4);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+        static int Add(int value)
+        {
+            //arr = new int[value];
+            return value;
+        }
+    }
+}
