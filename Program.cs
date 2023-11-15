2ea6d6a6d9060cd81949384fbcfe7b35b6f78835
HuseynovaDilara <huseynova.8885@gmail.com>
2023-11-15 20:44:27 +0400

    Class added

--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ namespace Task_14._11._23
     {
         static void Main(string[] args)
         {
-            
-            int[] arr = new int[] {1,2,3,4 };
-            //arr = arr.Add(1);
-            //arr = arr.Add(2);
-            //arr = arr.Add(3);
-            //arr = arr.Add(4);
+
+            int[] arr = new int[0];
+            arr = arr.Add(1);
+            arr = arr.Add(2);
+            arr = arr.Add(3);
+            arr = arr.Add(4);
             int sum = 0;
             foreach (var item in arr)
             {
@@ -19,10 +19,17 @@ namespace Task_14._11._23
             }
             Console.WriteLine(sum);
         }
-        static int Add(int value)
+        
+    }
+    static class Extention
+    {
+        public static int[] Add(this int[] arr, int item)
         {
-            //arr = new int[value];
-            return value;
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = item;
+            return arr;
         }
     }
 }
+
+
